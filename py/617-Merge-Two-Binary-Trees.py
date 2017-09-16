77bc9430318f2d377632b2faefc9aa98df4ae574
Chen <ggcmbpr@gmail.com>
2017-09-16 01:45:37 -0500

    617-Merge-Two-Binary-Trees.py
    regards to python grammar, in side function, if directly change function
    parameter, the effect won't go back to func caller. Only change
    parameter's member can take effect. because change a parameter means
    rebinding a new name with an object.
    
    Cause: None
    Solution: None
    Module: APP
    Fixed Version: None
    Branch: None
    Importance:
    Risk:

---
+++ py/617-Merge-Two-Binary-Trees.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+     def __init__(self, x):
+         self.val = x
+         self.left = None
+         self.right = None
+
+class Solution(object):
+    def mergeTrees(self, t1, t2):
+        """
+        :type t1: TreeNode
+        :type t2: TreeNode
+        :rtype: TreeNode
+        """
+        if t1 is None and t2 is not None:
+            return t2
+        elif t1 is not None and t2 is None:
+            return t1
+        elif ( t1 and t2 ) is None:
+            return None
+        else:
+            t1.val += t2.val
+        t1.left = self.mergeTrees( t1.left, t2.left )
+        t1.right = self.mergeTrees( t1.right, t2.right )
+        return t1
